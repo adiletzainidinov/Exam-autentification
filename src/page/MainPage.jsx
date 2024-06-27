bc769984be9801f5dad8755e09fe8ee7c4832391
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-06-27 16:30:17 +0600

    logout start

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const MainPage = () => {
   return (
-    <div>MainPage</div>
-  )
-}
+    <div>
+      MainPage
+      <div>logout</div>
+    </div>
+  );
+};
 
-export default MainPage
+export default MainPage;
